08fcb3bc607f5b6307417fcf6c410e5ccb849d52
Thilo von Neumann <tvn@mail.upb.de>
2021-05-11 08:49:03 +0200

    [Loss] Add thresholded SDR loss and module wrapper

--- graph_pit/loss/regression.py
+++ graph_pit/loss/regression.py
@@ -3,9 +3,17 @@ from padertorch.ops.losses.regression import _reduce
 import paderbox as pb
 
 
+__all__ = [
+    'sdr_loss',
+    'SDRLoss',
+    'thresholded_sdr_loss',
+    'ThresholdedSDRLoss',
+]
+
+
 @pb.utils.functional.partial_decorator
 def sdr_loss(estimate: torch.Tensor, target: torch.Tensor,
-             reduction: str = 'mean', aggregation: str = 'outside_log'):
+             reduction: str = 'mean', aggregation: str = 'none'):
     """
     The (scale dependent) SDR or SNR loss.
 
@@ -48,3 +56,96 @@ def sdr_loss(estimate: torch.Tensor, target: torch.Tensor,
         raise ValueError(f'Unknown aggregation type {aggregation}')
 
     return -_reduce(sdr, reduction=reduction)
+
+
+class SDRLoss(torch.nn.Module):
+    """
+    `partial_decorator` doesn't work with configurable (yet?), so we need this
+    class wrapper
+    """
+    def __init__(self, aggregation='outside_log', reduction='sum'):
+        super().__init__()
+        self.aggregation = aggregation
+        self.reduction = reduction
+
+    def forward(self, estimate, target):
+        sdr_loss(
+            estimate, target,
+            aggregation=self.aggregation,
+            reduction=self.reduction,
+        )
+
+
+def thresholded_sdr_loss(
+        estimate: torch.Tensor,
+        target: torch.Tensor,
+        threshold: float,
+        epsilon: float = 0,
+        reduction: str = 'sum',
+) -> torch.Tensor:
+    """
+    Thresholded SDR loss as defined in [1], eq. 2. If `epsilon > 0`, this
+    function computes the epsilon-tSDR defined in [2], eq. ?.
+
+    TODO: fill out the details when the paper is accepted
+
+    Args:
+        estimate:
+        target:
+        threshold: The threshold tau in [1], _not_ the max SDR! The max SDR can
+            be set with `threshold = 10**(-sdr_max / 10)`
+        epsilon: Epsilon from the epsilon-tSDR loss [2]. Makes the function
+            robust against perfect silence reconstruction.
+        reduction:
+
+    Returns:
+        The loss value
+
+    References:
+        [1] Wisdom, Scott, Efthymios Tzinis, Hakan Erdogan, Ron J. Weiss,
+            Kevin Wilson, and John R. Hershey. “Unsupervised Speech Separation
+            Using Mixtures of Mixtures.” In ICML 2020 Workshop on
+            Self-Supervision for Audio and Speech, 2020.
+            https://openreview.net/pdf?id=qMMzJGRPT2d
+
+        [2] Graph-PIT: Generalized permutation invariant training for
+            continuous separation of arbitrary numbers of speakers
+    """
+    target_power = torch.norm(target, dim=-1) ** 2 + epsilon
+    sdr = 10 * torch.log10(
+        target_power /
+        (
+                torch.norm(estimate - target, dim=-1) ** 2
+                + threshold * target_power
+        )
+    )
+    return -_reduce(sdr, reduction)
+
+
+class ThresholdedSDRLoss(torch.nn.Module):
+    def __init__(
+            self,
+            max_sdr: float,
+            epsilon: float = 0,
+            reduction: str = 'sum'
+    ):
+        """
+        Class variant of the thresholded SDR loss so that it can be used with
+        `pt.Configurable`. See docstring of `thresholded_sdr_loss` for details.
+
+        Args:
+            max_sdr: Used to set the threshold so that the minimum value of the
+                loss is limited to `-max_sdr`.
+        """
+        super().__init__()
+        self.threshold = 10 ** (-max_sdr / 10)
+        self.epsilon = epsilon
+        self.reduction = reduction
+
+    def __call__(
+            self, estimate: torch.Tensor, target: torch.Tensor
+    ) -> torch.Tensor:
+        return thresholded_sdr_loss(
+            estimate, target, self.threshold, self.epsilon, self.reduction
+        )
+
