b7e831c4488f9a5f16c325a4d2defec757aa0fd2
Thilo von Neumann <tvn@mail.upb.de>
2021-08-09 11:07:28 +0200

    [setup.py] Update requirements and extras_require

--- setup.py
+++ setup.py
@@ -1,14 +1,43 @@
+import itertools
 from distutils.core import setup
+
+extras_require = {
+    'example': [
+        'einops',
+        'sacred @ git+http://github.com/IDSIA/sacred',  # The PyPi version is not frequently updated
+    ],
+    'test': [
+        'pytest'
+    ],
+}
+extras_require['all'] = list(set(itertools.chain.from_iterable(
+    extras_require.values())
+))
 
 setup(
     name='graph_pit',
     version='0.1',
     packages=['graph_pit'],
-    install_requires=['torch'],
-    url='',
+    install_requires=[
+        'numpy',
+        'torch',
+        'cached_property',
+        'paderbox @ git+http://github.com/fgnt/paderbox',
+        'padertorch @ git+http://github.com/fgnt/padertorch',
+    ],
+    extras_require={
+        'example': [
+            'einops',
+            'sacred @ git+http://github.com/IDSIA/sacred',  # The PyPi version is not frequently updated
+        ],
+        'test': [
+            'pytest'
+        ],
+    },
+    url='https://github.com/fgnt/graph_pit',
     license='', # TODO
-    author='thequilo',
+    author='Thilo von Neumann',
     author_email='vonneumann@nt.upb.de',
     description='PyTorch implementation of the Graph-PIT objective for '
-                'training networks for continuous source separation'
+                'training networks for continuous source separation',
 )
