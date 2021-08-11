bc6aeb3db4af28fbee9664ab3995194b9375a3b8
Thilo von Neumann <tvn@mail.upb.de>
2021-08-11 10:28:17 +0200

    [Optimized Graph-PIT] Add base class for easier custom optimized Graph-PIT losses

--- graph_pit/loss/optimized.py
+++ graph_pit/loss/optimized.py
@@ -17,7 +17,59 @@ __all__ = [
 
 
 @dataclass
-class OptimizedGraphPITSourceAggregatedSDRLoss(GraphPITBase):
+class OptimizedGraphPITLoss(GraphPITBase):
+    """
+    Base class for optimized variants of Graph-PIT losses. Can be used to
+    define custom losses. Overwrite
+     - `similarity_matriy` to compute the matrix "M" from [1]
+     - `compute_f` to compute the function "f" from [1]
+
+    References:
+        [1] Speeding up Permutation Invariant Training
+    """
+    assignment_solver: Union[Callable, str] = 'optimal_brute_force'
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        if not callable(self.assignment_solver):
+            self.assignment_solver = (
+                graph_assignment_solvers[self.assignment_solver]
+            )(minimize=False)
+
+    @cached_property
+    def similarity_matrix(self) -> torch.Tensor:
+        """ The matrix "M" from the paper. """
+        raise NotImplementedError()
+
+    def compute_f(self, x) -> torch.Tensor:
+        """The function "f" from the paper."""
+        raise NotImplementedError()
+
+    @cached_property
+    def best_coloring(self) -> tuple:
+        similarity_matrix = self.similarity_matrix
+        x = self.assignment_solver(
+            pt.utils.to_numpy(similarity_matrix, detach=True), self.graph
+        )
+        assert x is not None
+        return tuple(x)
+
+    @cached_property
+    def max_similarity(self):
+        return torch.sum(
+            self.similarity_matrix[
+                np.arange(len(self.targets)), self.best_coloring
+            ]
+        )
+
+    @cached_property
+    def loss(self) -> torch.Tensor:
+        return self.compute_f(self.max_similarity)
+
+
+@dataclass
+class OptimizedGraphPITSourceAggregatedSDRLoss(OptimizedGraphPITLoss):
     """
     The sa-SDR with "optimized" assignment algorithms from [1]. The assignment
     solver can be selected by setting `permutation_solver` to one of
@@ -35,15 +87,6 @@ class OptimizedGraphPITSourceAggregatedSDRLoss(GraphPITBase):
     References:
         [1] Speeding up Permutation Invariant Training
     """
-    assignment_solver: Union[Callable, str] = 'optimal_brute_force'
-
-    def __post_init__(self):
-        super().__post_init__()
-
-        if not callable(self.assignment_solver):
-            self.assignment_solver = (
-                graph_assignment_solvers[self.assignment_solver]
-            )(minimize=False)
 
     @cached_property
     def similarity_matrix(self) -> torch.Tensor:
@@ -69,35 +112,16 @@ class OptimizedGraphPITSourceAggregatedSDRLoss(GraphPITBase):
         similarity_matrix = torch.stack(v)
         return similarity_matrix
 
-    @cached_property
-    def best_coloring(self) -> tuple:
-        similarity_matrix = self.similarity_matrix
-        x = self.assignment_solver(
-            pt.utils.to_numpy(similarity_matrix, detach=True), self.graph
-        )
-        assert x is not None
-        return tuple(x)
-
-    @cached_property
-    def loss(self) -> torch.Tensor:
+    def compute_f(self, x) -> torch.Tensor:
         # Compute target and estimate energies
         target_energy = torch.sum(
             torch.stack([torch.sum(t ** 2) for t in self.targets])
         )
         estimate_energy = torch.sum(self.estimate ** 2)
 
-        # Solve permutation problem based on similarity matrix
-        similarity = torch.sum(
-            self.similarity_matrix[
-                np.arange(len(self.targets)), self.best_coloring
-            ]
-        )
-
         # Compute the final SDR as
         # (|s|^2)/(|s|^2 + |shat|^2 + sum(matmul(s, shat)))
-        sdr = target_energy / (
-                target_energy + estimate_energy - 2 * similarity
-        )
+        sdr = target_energy / (target_energy + estimate_energy - 2 * x)
         return -10 * torch.log10(sdr)
 
 
@@ -116,7 +140,13 @@ def optimized_graph_pit_source_aggregated_sdr_loss(
     ).loss
 
 
-class OptimizedGraphPITSourceAggregatedSDRLossModule(LossModule):
+class OptimizedGraphPITLossModule(LossModule):
+    """
+    Base class for loss modules for Graph-PIT losses. Sub-class and set
+    `loss_class` to get a module for that loss.
+    """
+    loss_class = None
+
     def __init__(
             self,
             assignment_solver: Union[Callable, str] =
@@ -132,11 +162,21 @@ class OptimizedGraphPITSourceAggregatedSDRLossModule(LossModule):
     def get_loss_object(
             self, estimate: torch.Tensor, targets: torch.Tensor,
             segment_boundaries: List[Tuple[int, int]],
-    ) -> OptimizedGraphPITSourceAggregatedSDRLoss:
-        return OptimizedGraphPITSourceAggregatedSDRLoss(
+    ) -> loss_class:
+        return self.loss_class(
             estimate, targets, segment_boundaries,
             assignment_solver=self.assignment_solver
         )
 
     def extra_repr(self) -> str:
         return f'{self.assignment_solver}, '
+
+
+class OptimizedGraphPITSourceAggregatedSDRLossModule(
+    OptimizedGraphPITLossModule
+):
+    """
+    The sa-SDR Graph-PIT loss as a `Torch.nn.Module`. Can be used in a
+    torch module.
+    """
+    loss_class = OptimizedGraphPITSourceAggregatedSDRLoss
