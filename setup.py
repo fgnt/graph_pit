326847d3a84114ac2f431fed80e462ee9da75378
Thilo von Neumann <tvn@mail.upb.de>
2021-08-18 14:15:11 +0200

    [setup] Define "all" extras

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 from distutils.core import setup
+import itertools
 
+extras = {
+    'example': [
+        'einops',
+        'lazy_dataset',
+        # The PyPi version is not frequently updated
+        'sacred @ git+http://github.com/IDSIA/sacred',
+        'padertorch @ git+http://github.com/fgnt/padertorch',
+        'paderbox @ git+http://github.com/fgnt/paderbox',
+    ],
+    'test': [
+        'pytest',
+        'paderbox @ git+http://github.com/fgnt/paderbox',
+    ],
+}
+extras['all'] = list(set(itertools.chain.from_iterable(extras.values())))
 
 setup(
     name='graph_pit',
@@ -10,20 +26,7 @@ setup(
         'torch',
         'cached_property',
     ],
-    extras_require={
-        'example': [
-            'einops',
-            'lazy_dataset',
-            # The PyPi version is not frequently updated
-            'sacred @ git+http://github.com/IDSIA/sacred',
-            'padertorch @ git+http://github.com/fgnt/padertorch',
-            'paderbox @ git+http://github.com/fgnt/paderbox',
-        ],
-        'test': [
-            'pytest',
-            'paderbox @ git+http://github.com/fgnt/paderbox',
-        ],
-    },
+    extras_require=extras,
     url='https://github.com/fgnt/graph_pit',
     license='',  # TODO
     author='Thilo von Neumann',
